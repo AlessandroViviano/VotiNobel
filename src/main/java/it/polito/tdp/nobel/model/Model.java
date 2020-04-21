e7240b9ef1a7dd9f575cd084b5b367526e12e380
user <user@192.168.1.102>
2020-04-21 17:35:43 +0200

    finito

--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -1,15 +1,119 @@
 package it.polito.tdp.nobel.model;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-public class Model {
+import it.polito.tdp.nobel.db.EsameDAO;
 
+public class Model {
+	
+	private List<Esame> esami;
+	
+	private double bestMedia = 0.0;
+	private Set<Esame> bestSoluzione = null;
+	
+	public Model() {
+		EsameDAO dao = new EsameDAO();
+		this.esami = dao.getTuttiEsami();
+	}
 	
 	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
 		
-		System.out.println("TODO!");
+		Set<Esame> parziale = new HashSet<>();
 		
-		return null;
+		cerca2(parziale, 0, numeroCrediti);
+		
+		return bestSoluzione;
+	}
+	
+	//APPROCCIO 1	
+	//Complessità : 2^n
+	private void cerca1(Set<Esame> parziale, int L, int m) {
+		
+		
+		//casi terminali
+		
+		int crediti = sommaCrediti(parziale);
+		if(crediti>m)
+			return ;
+		
+		if(crediti==m) {
+			double media = calcolaMedia(parziale);
+			if(media>bestMedia) {
+				bestSoluzione = new HashSet<>(parziale);
+				bestMedia = media;
+			}
+		}
+		
+		//sicuramente, crediti minori < m
+		if(L==esami.size())
+			return ;
+		
+		//generiamo i sotto-problemi
+		//esami[L] è da aggiungere o no? Provo entrambe le cose
+		
+		//provo ad aggiungerlo
+		parziale.add(esami.get(L));
+		cerca1(parziale, L+1, m);
+		parziale.remove(esami.get(L));
+		
+		//provo a non aggiungerlo
+		cerca1(parziale, L+1, m);
+		
+		
+	}
+	
+	//APPROCCIO 2
+	//Complessità : n!
+	private void cerca2(Set<Esame> parziale, int L, int m) {
+		
+		//casi terminali
+		
+				int crediti = sommaCrediti(parziale);
+				if(crediti>m)
+					return ;
+				
+				if(crediti==m) {
+					double media = calcolaMedia(parziale);
+					if(media>bestMedia) {
+						bestSoluzione = new HashSet<>(parziale);
+						bestMedia = media;
+					}
+				}
+				
+				//sicuramente, crediti minori < m
+				if(L==esami.size())
+					return ;
+				
+				//generiamo i sotto-problemi
+				for(Esame e: esami) {
+					if(!parziale.contains(e)) {
+					parziale.add(e);
+					cerca2(parziale, L+1, m);
+					parziale.remove(e);
+					}
+				}
+		
+	}
+
+	private int sommaCrediti(Set<Esame> parziale) {
+		int somma = 0;
+		for(Esame e: parziale)
+			somma += e.getCrediti();
+		return somma;
+	}
+
+	public double calcolaMedia(Set<Esame> parziale) {
+		int crediti = 0;
+		int somma = 0;
+		
+		for(Esame e: parziale) {
+			crediti += e.getCrediti();
+			somma += (e.getVoto() * e.getCrediti());
+		}
+		
+		return somma/crediti;
 	}
 
 }
